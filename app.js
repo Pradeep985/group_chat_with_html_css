75cf50591a1c25c26539ebe609e14f58bed9fbcc
pradeep985 <pradeepyadav24th@gmail.com>
2025-02-12 00:10:03 +0530

    added routes

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-// Import Controllers
-const MessageController = require("./controllers/messageController");
-const ContactController = require("./controllers/contactController");
-
 const app = express();
 const PORT = 3000;
 
-// Middleware
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
+const contactRoutes = require("./routes/contactRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+
+app.use("/contactus", contactRoutes);
+app.use("/message", messageRoutes);
+
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/login.html"));
 app.get("/chat", (req, res) => res.sendFile(__dirname + "/views/index.html"));
-app.get("/contactus", (req, res) => res.sendFile(__dirname + "/views/contact.html"));
 app.get("/success", (req, res) => res.sendFile(__dirname + "/views/success.html"));
-
-app.post("/message", MessageController.saveMessage);
-app.post("/contactus", ContactController.saveContact);
 
 app.use((req, res) => {
     res.status(404).sendFile(__dirname + "/views/404.html");
